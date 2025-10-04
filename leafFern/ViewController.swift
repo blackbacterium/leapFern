648ef0f043a57950dbfc94d2c1cc1d37031b2fb8
Black Bacterium <blackbacterium@Blacks-MacBook-Air.local>
2025-10-04 20:50:44 +0300

    Добавил свое имя в ViewController

---
+++ leafFern/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  leafFern
+//
+//  Created by Black Bacterium on 04.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let myName = "Roman"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Меня зовут \(myName), я создал вое первое приложение")
+    }
+
+
+}
+
