65466719c23a67bb26c664da2cfd75f29394cb3e
Roman <pimkin-roma@mail.ru>
2025-10-17 21:16:45 +0300

    Добавил лейбл и кнопку

--- leafFern/ViewController.swift
+++ leafFern/ViewController.swift
@@ -8,21 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     private let helper = Helper()
+    private let userRepository = UserRepository()
+    private let textLabel = UILabel()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .green
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        view.alpha = 0.9
+        view.backgroundColor = .darkGray
+//        view.alpha = 1.0
         updatePersons()
+        
+        setupLabel()
+        view.addSubview(textLabel)
+        view.addSubview(button)
     }
     
     private func updatePersons() {
-        let userRepository = UserRepository()
         helper.addMuchUsers(users: userRepository.getUsers())
         
         for person in helper.getPersons() {
             print(person.personalInfo.fullName)
         }
+    }
+    
+    private func setupLabel() {
+        textLabel.text = 
+        "\(userRepository.getUsers().randomElement()?.personalInfo.fullName ?? "")"
+        textLabel.font = .systemFont(ofSize: 25, weight: .regular)
+        textLabel.textColor = .blue
+        textLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50) // fullName не видно
+//        textLabel.frame = CGRect(x: 100, y: 100, width: 200, height: 50) // fullName видно
     }
 }
